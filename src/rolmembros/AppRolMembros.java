41dd3661281edbf887f5d1de31b97c97c145077e
Andressa Varela <andressaavarela@gmail.com>
2023-04-19 13:43:16 -0300

    Delete JOption's inputs

--- src/rolmembros/AppRolMembros.java
+++ src/rolmembros/AppRolMembros.java
@@ -1,29 +1,23 @@
 package rolmembros;
 
-import javax.swing.JOptionPane;
-
 /**
  *
  * @author andre
  */
 public class AppRolMembros {
 
-    /**
-     Lista encadeada - Objetivo
-     * Visualizar a lista de membros da igreja, 
-     * remover ou adicionar membros e 
-     * visualizar e editar informações sobre eles.
-     */
     public static void main(String[] args) {
         ListaMembros lista;
         lista = new ListaMembros();
-        
+
         lista.adicionaMembro("Andressa Varela", "40028922", "07/11/2014");
         lista.adicionaMembro("Vanessa Varela", "198319203", "01/01/2010");
-        
-        
+
         lista.editaMembro("Andressa Varela", "38912389108", "39/12/2003");
         lista.imprimeListaMembros();
+
+        //Forma de adicionar infos por interface, a ignorar na APS
+        /*
         System.out.println("----");
         
         String entradaNome = 
@@ -35,7 +29,7 @@ public class AppRolMembros {
         
         lista.adicionaMembro(entradaNome, entradaTel, entradaData);
         lista.imprimeListaMembros();
-        
+         */
     }
-    
+
 }
