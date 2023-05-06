07a709761998e492a0334eb1ee320c5287a1d8f1
Andressa Varela <andressaavarela@gmail.com>
2023-05-06 12:32:58 -0300

    Edit console output

--- src/rolmembros/AppRolMembros.java
+++ src/rolmembros/AppRolMembros.java
@@ -1,4 +1,5 @@
 package rolmembros;
+
 /**
  *
  * @author andre
@@ -6,26 +7,31 @@ package rolmembros;
 public class AppRolMembros {
 
     public static void main(String[] args) {
-        ListaMembros lista;
-        lista = new ListaMembros();
+        ListaMembros lista = new ListaMembros();
 
+        System.out.println("LISTA DE MEMBROS:");
         lista.adicionaMembro("Andressa Varela", "40028922", "07/11/2014");
-        lista.adicionaMembro("Vanessa Varela", "198319203", "01/01/2010");
+        lista.adicionaMembro("Igor Marques", "198319203", "01/01/2010");
+        lista.adicionaMembro("José Maria", "11594513546", "11/08/1998");
+        lista.adicionaMembro("Maria José", "11940028922", "21/10/2018");
+        lista.imprimeListaMembros();
 
         lista.editaMembro("Andressa Varela", "38912389108", "39/12/2003");
+        System.out.println("-- após edição --");
+        System.out.println("LISTA DE MEMBROS:");
         lista.imprimeListaMembros();
-        
+
         PilhaDatas pilha = new PilhaDatas(5);
-        
+
         try {
-    pilha.adcdata(1);
-    pilha.adcdata(2);
-    pilha.adcdata(3);
-    pilha.adcdata(4);
-    System.out.println("Pilha após adicionar: " + pilha.toString());
-} catch (Exception e) {
-    System.out.println("Erro ao adicionar data: " + e.getMessage());
-}
+            pilha.adcdata(1);
+            pilha.adcdata(2);
+            pilha.adcdata(3);
+            pilha.adcdata(4);
+            System.out.println("Pilha após adicionar: " + pilha.toString());
+        } catch (Exception e) {
+            System.out.println("Erro ao adicionar data: " + e.getMessage());
+        }
         //Forma de adicionar infos por interface, a ignorar na APS
         /*
         System.out.println("----");
@@ -41,5 +47,5 @@ public class AppRolMembros {
         lista.imprimeListaMembros();
          */
     }
-        
+
 }
